486570802a27dc8ec1bb73c0648878a5824e2a5e
Cedric Florin <florincedric@gmail.com>
2026-02-26 01:43:54 +0800

    added models

--- commongrounds/localevents/models.py
+++ commongrounds/localevents/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from datetime import datetime
+from django.urls import reverse
 
 # Create your models here.
+class EventType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    
+    category = models.ForeignKey(
+        EventType,
+        on_delete=models.CASCADE
+    )
+
+    description = models.TextField()
+
+    location = models.CharField()
+
+    start_time = models.DateTimeField()
+
+    end_time = models.DateTimeField()
+
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    updated_on = models.DateTimeField(auto_now=True)
