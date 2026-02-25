55842d091a4082fd1c8519b55e243663c279924c
Hiroki <hiroki.watanabe@student.ateneo.edu>
2026-02-26 01:05:15 +0800

    created admin panels for models Book and Genre

--- commongrounds/bookclub/admin.py
+++ commongrounds/bookclub/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Book, Genre
 
-# Register your models here.
+class BookInLine(admin.TabularInline):
+    model = Book
+
+class GenreAdmin(admin.ModelAdmin):
+    model = Genre
+    search_fields = ('name',)
+    list_display = ('name', 'description')
+    inline = [BookInLine,]
+
+class BookAdmin(admin.ModelAdmin):
+    model = Book
+
+    search_fields = ('title','author',)
+    list_display = ('title', 'genre', 'author', 'publication_year', 'created_on','updated_on',)
+    list_filter = ('genre', 'publication_year', 'created_on','updated_on',)
+
+    fieldsets = [
+        ('Details', {
+            'fields':[
+                ('title','author','publication_year', 'created_on', 'updated_on',), 'genre'
+            ]
+        }),
+    ]
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Book, BookAdmin)
