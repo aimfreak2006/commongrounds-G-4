ebbcdee50a3cbdb17985443c72716c9a88b18994
Amir Ismael Mandanas <amirismaelmandanas@Amirs-MacBook-Air.local>
2026-02-28 21:43:40 +0800

    created the template html files

--- commongrounds/commissionrequests/models.py
+++ commongrounds/commissionrequests/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 # Must be sorted by name (ascending) .order_by(name)
 class CommissionType(models.Models):
     name = models.CharField(max_length=255)
     description = models.TextField()
+
+    def get_absolute_url(self):
+        return reverse('commissionrequests:detail_view', args=[int(self.id)])
 
 # Must be sorted by the date it was created (ascending) .order_by(created_on)
 class Commission(models.Models):
