da4709522a5b2579657770cd1e55a38d286325e1
calilaceron <calil.aceron@student.ateneo.edu>
2026-04-17 21:14:35 +0800

    fixes password reset URLs and adds email backend for password reset

--- commongrounds/commongrounds/settings.py
+++ commongrounds/commongrounds/settings.py
@@ -123,10 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+LOGIN_URL = '/accounts/login'
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
