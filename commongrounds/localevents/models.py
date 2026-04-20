f71246ea645e7e5d3b0510fbc184e2c6e70d146f
Cedric Florin <florincedric@gmail.com>
2026-04-21 02:00:42 +0800

    updated models

--- commongrounds/localevents/models.py
+++ commongrounds/localevents/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 
+from accounts.models import Profile
+
 
 class EventType(models.Model):
     name = models.CharField(max_length=255)
@@ -22,13 +24,39 @@ class Event(models.Model):
     category = models.ForeignKey(
         EventType,
         on_delete=models.CASCADE,
+        related_name='events',
+        editable=False,
+        null=True,
+        blank=True,
+    )
+
+    organizer = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
         related_name='events'
+    )
+    event_image = models.ImageField(
+        upload_to='event_images/',
+        null=True,
+        blank=True
     )
 
     description = models.TextField()
     location = models.CharField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+
+    event_capacity = models.IntegerField()
+    status = models.CharField(
+        choices=[
+            ('Available', 'Available'),
+            ('Full', 'Full'),
+            ('Done', 'Done'),
+            ('Cancelled', 'Cancelled'),
+        ],
+        default='Available'
+    )
+
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -43,3 +71,22 @@ class Event(models.Model):
         unique_together = ['title', 'created_on',]
         verbose_name = 'event'
         verbose_name_plural = 'events'
+
+
+class EventSignup(models.Model):
+    event = models.ForeignKey(
+        Event,
+        on_delete=models.CASCADE,
+        related_name='event signups',
+        null=True,
+        blank=True,
+    )
+    user_registrant = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        
+    )
+    new_registrant = models.CharField(
+        null=True, 
+        blank=True,
+        )
