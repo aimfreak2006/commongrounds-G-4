005f60f0357077a4a4996345bed553ca5919b994
Benedict Benj Laurente <benedict.benj.laurente@student.ateneo.edu>
2026-04-25 17:00:49 +0800

    Add the necessary url paths for new CBVs.

--- commongrounds/diyprojects/urls.py
+++ commongrounds/diyprojects/urls.py
@@ -1,17 +1,26 @@
 from django.urls import path
-from .views import ProjectCategoryListView, ProjectDetailView
+from .views import (ProjectCategoryListView, 
+                    ProjectDetailView,
+                    ProjectCreateView,
+                    ProjectUpdateView)
 
 urlpatterns = [
     path(
-     'projects/',
-     ProjectCategoryListView.as_view(),
-     name='all-projects'
-     ),
+      'projects/',
+      ProjectCategoryListView.as_view(),
+      name='all-projects'),
     path(
-     'project/<int:pk>',
-     ProjectDetailView.as_view(),
-     name='project-details'
-     ),
+      'project/<int:pk>',
+      ProjectDetailView.as_view(),
+      name='project-details'),
+    path(
+      'project/add',
+      ProjectCreateView.as_view(), 
+      name='project-add'),
+    path(
+      'project/<int:pk>/edit',
+      ProjectUpdateView.as_view(),
+      name='project-update'),
 ]
 
 app_name = "diyprojects"
