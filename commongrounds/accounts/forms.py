f720023af23573337e9aacf7d1705c090f6a929a
Benedict Benj Laurente <benedict.benj.laurente@student.ateneo.edu>
2026-05-03 16:35:56 +0800

    Add new class-based forms to have placeholders for inputs on registration and profile update.

--- commongrounds/accounts/forms.py
+++ commongrounds/accounts/forms.py
@@ -10,9 +10,49 @@ class ProfileUpdateForm(forms.ModelForm):
         fields = ['display_name']
 
 
+class CustomProfileUpdateForm(ProfileUpdateForm):   
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.fields['display_name'].label = ''
+        self.fields['display_name'].help_text = ''
+        self.fields['display_name'].widget.attrs.update({
+            'placeholder': 'Display Name'
+        })
+
+
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class CustomRegisterForm(RegisterForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'].label = ''
+        self.fields['username'].help_text = '' 
+        self.fields['username'].widget.attrs.update({
+            'placeholder': 'Username'
+        })
+
+        self.fields['email'].label = ''
+        self.fields['email'].help_text = '' 
+        self.fields['email'].widget.attrs.update({
+            'placeholder': 'Email'
+        })
+
+        self.fields['password1'].label = ''
+        self.fields['password1'].help_text = '' 
+        self.fields['password1'].widget.attrs.update({
+            'placeholder': 'Password'
+        })
+        
+        self.fields['password2'].label = ''
+        self.fields['password2'].help_text = ''
+        self.fields['password2'].widget.attrs.update({
+            'placeholder': 'Confirm Password'
+        })
