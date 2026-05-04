bf095dc399c9507656b5199cd4e6eea05616ca0c
Benedict Benj Laurente <benedict.benj.laurente@student.ateneo.edu>
2026-05-04 22:03:17 +0800

    Add placeholders for input boxes in localevents app and merchstore app.

--- commongrounds/localevents/forms.py
+++ commongrounds/localevents/forms.py
@@ -11,6 +11,53 @@ class EventCreateForm(forms.ModelForm):
         ]
 
 
+class CustomEventCreateForm(EventCreateForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['title'].label = ''
+        self.fields['title'].help_text = '' 
+        self.fields['title'].widget.attrs.update({
+            'placeholder': 'Event Title'
+        })
+
+        self.fields['category'].label = ''
+        self.fields['category'].help_text = '' 
+        self.fields['category'].widget.attrs.update({
+            'placeholder': 'Event Category'
+        })
+        
+        self.fields['description'].label = ''
+        self.fields['description'].help_text = ''
+        self.fields['description'].widget.attrs.update({
+            'placeholder': 'Event Description'
+        })
+
+        self.fields['location'].label = ''
+        self.fields['location'].help_text = ''
+        self.fields['location'].widget.attrs.update({
+            'placeholder': 'Event Location'
+        })
+
+        self.fields['start_time'].label = ''
+        self.fields['start_time'].help_text = ''
+        self.fields['start_time'].widget.attrs.update({
+            'placeholder': 'Event Start Time'
+        })
+
+        self.fields['end_time'].label = ''
+        self.fields['end_time'].help_text = ''
+        self.fields['end_time'].widget.attrs.update({
+            'placeholder': 'Event End Time'
+        })
+
+        self.fields['event_capacity'].label = ''
+        self.fields['event_capacity'].help_text = ''
+        self.fields['event_capacity'].widget.attrs.update({
+            'placeholder': 'Event Capacity'
+        })
+        
+
 class EventUpdateForm(forms.ModelForm):
     class Meta:
         model = Event
@@ -19,6 +66,53 @@ class EventUpdateForm(forms.ModelForm):
         'description', 'location', 'start_time', 
         'end_time', 'event_capacity', 'status',
         ]
+
+
+class CustomEventUpdateForm(EventUpdateForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['title'].label = ''
+        self.fields['title'].help_text = '' 
+        self.fields['title'].widget.attrs.update({
+            'placeholder': 'Event Title'
+        })
+
+        self.fields['category'].label = ''
+        self.fields['category'].help_text = '' 
+        self.fields['category'].widget.attrs.update({
+            'placeholder': 'Event Category'
+        })
+        
+        self.fields['description'].label = ''
+        self.fields['description'].help_text = ''
+        self.fields['description'].widget.attrs.update({
+            'placeholder': 'Event Description'
+        })
+
+        self.fields['location'].label = ''
+        self.fields['location'].help_text = ''
+        self.fields['location'].widget.attrs.update({
+            'placeholder': 'Event Location'
+        })
+
+        self.fields['start_time'].label = ''
+        self.fields['start_time'].help_text = ''
+        self.fields['start_time'].widget.attrs.update({
+            'placeholder': 'Event Start Time'
+        })
+
+        self.fields['end_time'].label = ''
+        self.fields['end_time'].help_text = ''
+        self.fields['end_time'].widget.attrs.update({
+            'placeholder': 'Event End Time'
+        })
+
+        self.fields['event_capacity'].label = ''
+        self.fields['event_capacity'].help_text = ''
+        self.fields['event_capacity'].widget.attrs.update({
+            'placeholder': 'Event Capacity'
+        })
 
 
 class EventSignupForm(forms.ModelForm):
--- commongrounds/merchstore/forms.py
+++ commongrounds/merchstore/forms.py
@@ -1,8 +1,50 @@
 from django import forms
-from .models import Transaction
+from .models import Transaction, Product
 
 
 class TransactionForm(forms.ModelForm):
     class Meta:
         model = Transaction
         fields = ["amount"]
+
+
+class CustomProductCreateForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = [
+            "name", "product_type", "product_image",
+            "description", "price", "stock", "status"
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['name'].label = ''
+        self.fields['name'].help_text = '' 
+        self.fields['name'].widget.attrs.update({
+            'placeholder': 'Product Name'
+        })
+
+        self.fields['product_type'].label = ''
+        self.fields['product_type'].help_text = '' 
+        self.fields['product_type'].widget.attrs.update({
+            'placeholder': 'Category'
+        })
+        
+        self.fields['description'].label = ''
+        self.fields['description'].help_text = ''
+        self.fields['description'].widget.attrs.update({
+            'placeholder': 'Product Description'
+        })
+
+        self.fields['price'].label = ''
+        self.fields['price'].help_text = ''
+        self.fields['price'].widget.attrs.update({
+            'placeholder': 'Set Product Price'
+        })
+
+        self.fields['stock'].label = ''
+        self.fields['stock'].help_text = ''
+        self.fields['stock'].widget.attrs.update({
+            'placeholder': 'Current Stock'
+        })
